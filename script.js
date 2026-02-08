6bb4517424f5036c2cfdac22d665c03b074f5cac
02camilasoares-hash <02camila.soares@gmail.com>
2026-02-08 00:24:55 -0300

    js atualizado

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-gsap.registerPlugin(ScrollTrigger,ScrollSmoother,SplitText);
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother, SplitText);
 
 ScrollSmoother.create({
 	smooth: 1, // how long (in seconds) it takes to "catch up" to the native scroll position
 	effects: true, // looks for data-speed and data-lag attributes on elements
 	// smoothTouch: 0.1  much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
-});
+})
 
 gsap.from(".hero", {
     opacity: 0
@@ -20,6 +20,76 @@ gsap.fromTo(".pessoinhas",{ //solução pelo chat gpt pra arrumar a imagem que f
   { y: 0, duration: 1 }
 )
 
-gsap.from(".cidades" {
-    x:50,
+
+gsap.from(".cidades > div", { //especificar que quero usar todos os 'filhos' da div cidades
+  y: 50,
+  opacity: 0,
+  duration: 1,
+  filter: "blur (20px)",
+  stagger: .3,
+  scrollTrigger: {
+    trigger: ".cidades > div",
+    start: "0% 80%",
+    end: "100% 70%",
+    scrub: true,
+  }
 })
+
+gsap.from(".agradecimento > ul > li", {
+  opacity: 0,
+  x: 50,
+  filter: "blur(20px)",
+  stagger: .1,
+  scrollTrigger: {
+    trigger: "li",
+    start: "0% 80%",
+    end: "100% 50%",
+    scrub: true,
+  }
+})
+
+gsap.from ("footer", {
+  y: "-30%",
+  scrollTrigger: {
+    trigger: "footer",
+    scrub: true,
+    invalidadeOnRefresh: true,
+    end: "100% 100%"
+  }
+})
+
+const grupoTextoSplit = document.querySelectorAll (".textosplit"); //slecione tds elementos da pag q tem a clase do texto
+// animar cada elemento desse agrupamtneot - forEach
+
+grupoTextoSplit.forEach((textoUnicoSplit) => {
+  const split = SplitText.create(textoUnicoSplit, {
+    type: "lines, words, chars",
+    mask: "lines",
+  });
+
+  gsap.from(split.chars, {
+    y: 40, 
+    opacity: 0,
+    duration: 0.3,
+    stagger: 0.03,
+    scrollTrigger: {
+      trigger: textoUnicoSplit,
+      
+    }
+  });
+});
+
+
+  
+
+
+//SplitText.create(.textosplit, {
+//  type: "lines, words, chars", //corta o texto em linhas, palavas e caracteres
+ // mask: "lines" //mascara o texto abaixo das linhas
+
+
+// gsap.from(split.chars, {
+  //y: 40,
+ // opacity: 0,
+  //duration: .2,
+  //stagger: .5,
